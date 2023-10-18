4d8acd517333491293cbcabbff88eb99bf0c73d0
Supratim Mondal <you@example.com>
2023-10-18 16:11:15 +0530

    updated logic

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function convert() {
     const evenSGPA = parseFloat(document.getElementById("evenSGPA").value);
     const oddSGPA = parseFloat(document.getElementById("oddSGPA").value);
 
-    if (isNaN(evenSGPA) || isNaN(oddSGPA)) {
-        document.getElementById("result").textContent = "Please enter valid SGPA values.";
+    if (isNaN(evenSGPA) || isNaN(oddSGPA) || evenSGPA < 5 || evenSGPA > 10 || oddSGPA < 5 || oddSGPA > 10) {
+        document.getElementById("result").textContent = "Please enter valid SGPA values between 5.0 and 10.";
     } else {
         const ygpa = (evenSGPA + oddSGPA) / 2; // Average of even and odd semester SGPA
         const percentage = ((ygpa - 0.75) * 10).toFixed(2);
