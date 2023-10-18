4863832d44bee6d90883e6bf24184104a7f85c7e
Supratim Mondal <you@example.com>
2023-10-18 15:48:36 +0530

    2nd; updated ui

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function convert() {
     } else {
         const ygpa = (evenSGPA + oddSGPA) / 2; // Average of even and odd semester SGPA
         const percentage = ((ygpa - 0.75) * 10).toFixed(2);
-        const result = `Your YGPA is: ${ygpa.toFixed(2)}<br>Your Percentage is: ${percentage}%`;
+        const result = `<b>Your YGPA is: ${ygpa.toFixed(2)}<br>Your Percentage is: ${percentage}%</b><br><br>Made with 💙 by Supratim`;
         document.getElementById("result").innerHTML = result;
     }
 }
